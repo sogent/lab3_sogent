5d63016de9866ae0b8269b3d1328e7d66a782ff4
soge-commits <sogent@mail.umkc.edu>
2021-09-29 16:45:05 -0500

    program is nearly complete, just need to fix formatting

---
+++ cmake-build-debug/CMakeFiles/lab3_sogent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_sogent.dir/Lab03.cpp.o"
+  "CMakeFiles/lab3_sogent.dir/checkingmodule.cpp.o"
+  "lab3_sogent"
+  "lab3_sogent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_sogent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
