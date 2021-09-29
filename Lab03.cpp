dc608892602864aec98dec572d53eff50f74cf00
soge-commits <sogent@mail.umkc.edu>
2021-09-29 18:36:54 -0500

    program is complete

--- Lab03.cpp
+++ Lab03.cpp
@@ -52,11 +52,11 @@ int main(){
                 userLoan =calcLoanAmount(userLoanPrincipal, userInterestRate, userLoanTerm);
                 userInterestPercent=userInterestRate*100;
                 cout<<setw(30)<<"Loan Options Review"<<endl;
-                cout<<"Interest Rate: " <<right<<setw(24)<<setprecision(1)<<userInterestPercent<<"%"<<endl;
+                cout<<"Interest Rate: " <<right<<setw(24)<<userInterestPercent<<"%"<<endl;
                 cout<<"Principal: "<<right<<setw(24)<<"$"<<userLoanPrincipal<<endl;
                 cout<<"Loan Monthly Term: "<<setw(14)<<userLoanTerm<<" months"<<endl;
                 cout<<endl;
-                cout<<"Your total loan amount is: "<<setw(3)<<setprecision(2)<<fixed<<"$"<<userLoan<<endl;
+                cout<<"Your total loan amount is: "<<setw(5)<<setprecision(2)<<fixed<<"$"<<userLoan<<endl;
                 cout<<endl;
                 break;
             case 'S':
